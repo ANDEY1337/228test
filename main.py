2ba9009343a1b4e2072f8c4d8a3c8c62f02a0228
ANDEY1337 <92263559+ANDEY1337@users.noreply.github.com>
2021-10-14 12:40:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 from kivy.app import App
-from kivy.uix.label import Label
- 
-class MainApp(App):
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+import time
+Builder.load_string('''
+<CameraClick>:
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.capture()
+''')
+
+
+class CameraClick(BoxLayout):
+    def capture(self):
+        '''
+        Function to capture the images and give them the names
+        according to their captured time and date.
+        '''
+        camera = self.ids['camera']
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        camera.export_to_png("IMG_{}.png".format(timestr))
+        print("Captured")
+
+
+class TestCamera(App):
+
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
- 
-        return label
- 
-if __name__ == '__main__':
-    app = MainApp()
-    app.run()
+        return CameraClick()
+
+
+TestCamera().run()
